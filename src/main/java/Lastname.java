959553839d13ad26de716f4fb1e1e07a42707bae
shubhambhusari <shubhambhusari2@gmail.com>
2021-03-03 18:08:05 +0530

    Added:to validate firstname ,lastname,email,mobile,password

--- src/main/java/Lastname.java
+++ src/main/java/Lastname.java
@@ -3,14 +3,35 @@ import java.util.regex.Pattern;
 
     public class Lastname {
 
-        private static final String EMAIL= "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        private static String firstName = "^[A-Z]{1}[a-z]{2,14}$";
+        private static String lastName = "^[A-Z]{1}[a-z]{2,14}$";
+        private static String email_ = "^[_A-Za-z0-9-+]+(\\.[A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        private static String phoneNumber = "^[0-9]{2}[ ][6-9]{1}[0-9]{9}$";
+        private static String  PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,}";
 
 
-        public boolean checkEmail(String email) {
-            Pattern pattern = Pattern.compile(email);
-            return Pattern.matches(EMAIL,email);
 
+        public boolean checkFirstName(String name){
+            Pattern p = Pattern.compile(name);
+            return Pattern.matches(firstName, name);
         }
-
+        public boolean checkLastName(String name){
+            Pattern p = Pattern.compile(name);
+            return Pattern.matches(lastName, name);
         }
+        public boolean checkEmail(String email){
+            Pattern p =Pattern.compile(email);
+            return Pattern.matches(email_, email);
+        }
+        public boolean checkPhoneNumber(String phoneNum){
+            Pattern p = Pattern.compile(phoneNum);
+            return Pattern.matches(phoneNumber, phoneNum);
+        }
+        public boolean checkPassword(String password){
+            Pattern p = Pattern.compile(password);
+            return Pattern.matches(PASSWORD, password);
+        }
+    }
+
+
 
--- src/test/java/UserValidation.java
+++ src/test/java/UserValidation.java
@@ -2,19 +2,90 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 public class UserValidation {
-
-    Lastname operation;
-
+    UserValidation operation;
     @BeforeEach
-    public void setUp(){
-        operation = new Lastname();
-
+    public void setUp() {
+        operation = new UserValidation();
     }
 
-   @Test
-    public void testmobile(){
-    Assertions.assertTrue(operation.checkEmail("shubhambhusari@gmail.com"));
-    }
-}
+    @Test
+
+        public void testFirstName_true() {
+            boolean result;
+            result = operation.checkFirstName("Shubham");
+            System.out.println(result);
+            Assertions.assertTrue(result);
+        }
+
+    @Test
+        public void   testFirstName_false() {
+            boolean result;
+            result = operation.checkFirstName("Shubham");
+            System.out.println(result);
+            Assertions.assertFalse(result);
+        }
+
+        @Test
+        public void testLastName_true() {
+                boolean result;
+                result = operation.checkLastName("Bhusari");
+                System.out.println(result);
+                Assertions.assertTrue(result);
+            }
+            public void testLastName_false() {
+                boolean result;
+                result = operation.checkLastName("bhusari");
+                System.out.println(result);
+                Assertions.assertFalse(result);
+            }
+
+            @Test
+
+                public void checkEmail_true() {
+                    boolean result;
+                    result = operation.checkEmail("shubhambhusari2@gmail.com");
+                    System.out.println(result);
+                    Assertions.assertTrue(result);
+                }
+                @Test
+                public void checkEmail_false() {
+                    boolean result;
+                    result = operation.checkEmail("shubhambhusari2.com");
+                    System.out.println(result);
+                    Assertions.assertFalse(result);
+                }
+
+                @Test
+
+                    public void checkPhoneNumber_true () {
+                        boolean result;
+                        result = operation.checkPhoneNumber("91 8888937397");
+                        System.out.println(result);
+                        Assertions.assertTrue(result);
+                    }
+                    @Test
+                    public void checkPhoneNumber_false () {
+                        boolean result;
+                        result = operation.checkPhonenumber("888937397");
+                        System.out.println(result);
+                        Assertions.assertFalse(result);
+                    }
+
+                    @Test
+
+                    public void checkPassword_true () {
+                        boolean result;
+                        result = operation.checkPassword("Shubh@123");
+                        System.out.println(result);
+                        Assertions.assertTrue(result);
+                    }
+                    @Test
+                    public void checkPassword_false () {
+
+                        boolean result;
+                        result = operation.checkPassword("@123");
+                        System.out.println(result);
+                        Assertions.assertFalse(result);
+                    }
+                }
